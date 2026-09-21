47303bba3f59a8165531eea936ec3298bd3e72e4
Christian Rémillard <christian.remillard@gmail.com>
2026-09-21 16:10:05 -0400

    wip

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 import svgrPlugin from 'vite-plugin-svgr'
 
 export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
   envPrefix: ['VITE_', 'REACT_APP_'],
   plugins: [
     react({
